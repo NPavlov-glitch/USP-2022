fdee75858202275753571a4ba5983e7328e3afcd
unknown <npavlov@2create.studio>
2022-05-25 21:39:16 +0300

    Add form styles

---
+++ templates/filter.php
@@ -0,0 +1,85 @@
+<div class="filter">
+	<h2>Филтри</h2>
+
+	<form>
+		<ul>
+			<li>
+				<label>Марка:</label>
+			</li>
+
+			<li>
+				<select type="select" name="brand" class="dropdown">
+					<option value="volvo">Volvo</option>
+					<option value="saab">Saab</option>
+					<option value="opel">Opel</option>
+					<option value="audi">Audi</option>
+				</select>
+			</li>
+
+			<li>
+				<label>Модел:</label>
+			</li>
+
+			<li>
+				<select type="select" name="model" class="dropdown">
+					<option value="volvo">Volvo</option>
+					<option value="saab">Saab</option>
+					<option value="opel">Opel</option>
+					<option value="audi">Audi</option>
+				</select>
+			</li>
+
+			<li>
+				<label>Екран:</label>
+			</li>
+
+			<li>
+				<select type="select" name="screen" class="dropdown">
+					<option value="volvo">Volvo</option>
+					<option value="saab">Saab</option>
+					<option value="opel">Opel</option>
+					<option value="audi">Audi</option>
+				</select>
+			</li>
+
+			<li>
+				<label>Камера:</label>
+			</li>
+
+			<li>
+				<select type="select" name="camera" class="dropdown">
+					<option value="volvo">Volvo</option>
+					<option value="saab">Saab</option>
+					<option value="opel">Opel</option>
+					<option value="audi">Audi</option>
+				</select>
+			</li>
+
+			<li>
+				<label>Памет:</label>
+			</li>
+
+			<li>
+				<select type="select" name="memory" class="dropdown">
+					<option value="volvo">Volvo</option>
+					<option value="saab">Saab</option>
+					<option value="opel">Opel</option>
+					<option value="audi">Audi</option>
+				</select>
+			</li>
+
+			<li>
+				<label>Цена :</label>
+			</li>
+
+			<li>
+				<select type="select" name="price" class="dropdown">
+					<option value="volvo">Volvo</option>
+					<option value="saab">Saab</option>
+					<option value="opel">Opel</option>
+					<option value="audi">Audi</option>
+				</select>
+			</li>
+		</ul>
+	</form>
+</div>
---
+++ templates/listing.php
@@ -0,0 +1,154 @@
+<div class="items">
+	<div class="phone">
+		<div class="phone_img">
+			<img src="img/phone1.jpg">
+		</div>
+		
+		<div class="phone_txt">
+			<ul>
+				<li>Марка: Xiaomi</li>
+				<li>Модел: Redmi Note 11 Pro</li>
+				<li>Екран: 6.67"</li>
+				<li>Камера: 108 MP</li>
+				<li>Памет: 128 GB</li>
+				<li>Цена: 500 лв</li>
+			</ul>
+		</div>
+	</div>
+
+	<div class="phone">
+		<div class="phone_img">
+			<img src="img/phone1.jpg">
+		</div>
+		
+		<div class="phone_txt">
+			<ul>
+				<li>Марка: Xiaomi</li>
+				<li>Модел: Redmi Note 11 Pro</li>
+				<li>Екран: 6.67"</li>
+				<li>Камера: 108 MP</li>
+				<li>Памет: 128 GB</li>
+				<li>Цена: 500 лв</li>
+			</ul>
+		</div>
+	</div>
+
+	<div class="phone">
+		<div class="phone_img">
+			<img src="img/phone1.jpg">
+		</div>
+		
+		<div class="phone_txt">
+			<ul>
+				<li>Марка: Xiaomi</li>
+				<li>Модел: Redmi Note 11 Pro</li>
+				<li>Екран: 6.67"</li>
+				<li>Камера: 108 MP</li>
+				<li>Памет: 128 GB</li>
+				<li>Цена: 500 лв</li>
+			</ul>
+		</div>
+	</div>
+
+	<div class="phone">
+		<div class="phone_img">
+			<img src="img/phone1.jpg">
+		</div>
+		
+		<div class="phone_txt">
+			<ul>
+				<li>Марка: Xiaomi</li>
+				<li>Модел: Redmi Note 11 Pro</li>
+				<li>Екран: 6.67"</li>
+				<li>Камера: 108 MP</li>
+				<li>Памет: 128 GB</li>
+				<li>Цена: 500 лв</li>
+			</ul>
+		</div>
+	</div>
+
+	<div class="phone">
+		<div class="phone_img">
+			<img src="img/phone1.jpg">
+		</div>
+		
+		<div class="phone_txt">
+			<ul>
+				<li>Марка: Xiaomi</li>
+				<li>Модел: Redmi Note 11 Pro</li>
+				<li>Екран: 6.67"</li>
+				<li>Камера: 108 MP</li>
+				<li>Памет: 128 GB</li>
+				<li>Цена: 500 лв</li>
+			</ul>
+		</div>
+	</div>
+
+	<div class="phone">
+		<div class="phone_img">
+			<img src="img/phone1.jpg">
+		</div>
+		
+		<div class="phone_txt">
+			<ul>
+				<li>Марка: Xiaomi</li>
+				<li>Модел: Redmi Note 11 Pro</li>
+				<li>Екран: 6.67"</li>
+				<li>Камера: 108 MP</li>
+				<li>Памет: 128 GB</li>
+				<li>Цена: 500 лв</li>
+			</ul>
+		</div>
+	</div>
+
+	<div class="phone">
+		<div class="phone_img">
+			<img src="img/phone1.jpg">
+		</div>
+		
+		<div class="phone_txt">
+			<ul>
+				<li>Марка: Xiaomi</li>
+				<li>Модел: Redmi Note 11 Pro</li>
+				<li>Екран: 6.67"</li>
+				<li>Камера: 108 MP</li>
+				<li>Памет: 128 GB</li>
+				<li>Цена: 500 лв</li>
+			</ul>
+		</div>
+	</div>
+
+	<div class="phone">
+		<div class="phone_img">
+			<img src="img/phone1.jpg">
+		</div>
+		
+		<div class="phone_txt">
+			<ul>
+				<li>Марка: Xiaomi</li>
+				<li>Модел: Redmi Note 11 Pro</li>
+				<li>Екран: 6.67"</li>
+				<li>Камера: 108 MP</li>
+				<li>Памет: 128 GB</li>
+				<li>Цена: 500 лв</li>
+			</ul>
+		</div>
+	</div>
+
+	<div class="phone">
+		<div class="phone_img">
+			<img src="img/phone1.jpg">
+		</div>
+		
+		<div class="phone_txt">
+			<ul>
+				<li>Марка: Xiaomi</li>
+				<li>Модел: Redmi Note 11 Pro</li>
+				<li>Екран: 6.67"</li>
+				<li>Камера: 108 MP</li>
+				<li>Памет: 128 GB</li>
+				<li>Цена: 500 лв</li>
+			</ul>
+		</div>
+	</div>
+</div>
