eb23a5bc4cf8870390191fc0b19e5579aec7f4fc
unknown <npavlov@2create.studio>
2022-05-25 22:04:12 +0300

    Add Listing integration

--- config/db_connect.php
+++ config/db_connect.php
@@ -5,4 +5,3 @@ $db_connection = mysqli_connect('localhost', 'root', 'kuku', 'usp_mobile_devices
 if( ! $db_connection ){
     echo 'Conncetion error: ' . mysqli_connect_error();
 }
-?>
--- templates/listing.php
+++ templates/listing.php
@@ -1,154 +1,48 @@
+<?php
+$sql = "SELECT * FROM devices";
+    
+$result = mysqli_query( $db_connection, $sql );
+
+$devices = mysqli_fetch_all( $result, MYSQLI_ASSOC );
+
+mysqli_free_result( $result );
+
+mysqli_close( $db_connection );
+?>
 <div class="items">
-	<div class="phone">
-		<div class="phone_img">
-			<img src="img/phone1.jpg">
+	<?php foreach ( $devices as $device ) : ?>
+		<div class="phone">
+			<div class="phone_img">
+				<img src="img/phone1.jpg">
+			</div>
+			
+			<div class="phone_txt">
+				<ul>
+					<li>
+						Марка: Xiaomi
+					</li>
+					
+					<li>
+						Модел: Redmi Note 11 Pro
+					</li>
+					
+					<li>
+						Екран: 6.67"
+					</li>
+					
+					<li>
+						Камера: 108 MP
+					</li>
+					
+					<li>
+						Памет: 128 GB
+					</li>
+					
+					<li>
+						Цена: 500 лв
+					</li>
+				</ul>
+			</div>
 		</div>
-		
-		<div class="phone_txt">
-			<ul>
-				<li>Марка: Xiaomi</li>
-				<li>Модел: Redmi Note 11 Pro</li>
-				<li>Екран: 6.67"</li>
-				<li>Камера: 108 MP</li>
-				<li>Памет: 128 GB</li>
-				<li>Цена: 500 лв</li>
-			</ul>
-		</div>
-	</div>
-
-	<div class="phone">
-		<div class="phone_img">
-			<img src="img/phone1.jpg">
-		</div>
-		
-		<div class="phone_txt">
-			<ul>
-				<li>Марка: Xiaomi</li>
-				<li>Модел: Redmi Note 11 Pro</li>
-				<li>Екран: 6.67"</li>
-				<li>Камера: 108 MP</li>
-				<li>Памет: 128 GB</li>
-				<li>Цена: 500 лв</li>
-			</ul>
-		</div>
-	</div>
-
-	<div class="phone">
-		<div class="phone_img">
-			<img src="img/phone1.jpg">
-		</div>
-		
-		<div class="phone_txt">
-			<ul>
-				<li>Марка: Xiaomi</li>
-				<li>Модел: Redmi Note 11 Pro</li>
-				<li>Екран: 6.67"</li>
-				<li>Камера: 108 MP</li>
-				<li>Памет: 128 GB</li>
-				<li>Цена: 500 лв</li>
-			</ul>
-		</div>
-	</div>
-
-	<div class="phone">
-		<div class="phone_img">
-			<img src="img/phone1.jpg">
-		</div>
-		
-		<div class="phone_txt">
-			<ul>
-				<li>Марка: Xiaomi</li>
-				<li>Модел: Redmi Note 11 Pro</li>
-				<li>Екран: 6.67"</li>
-				<li>Камера: 108 MP</li>
-				<li>Памет: 128 GB</li>
-				<li>Цена: 500 лв</li>
-			</ul>
-		</div>
-	</div>
-
-	<div class="phone">
-		<div class="phone_img">
-			<img src="img/phone1.jpg">
-		</div>
-		
-		<div class="phone_txt">
-			<ul>
-				<li>Марка: Xiaomi</li>
-				<li>Модел: Redmi Note 11 Pro</li>
-				<li>Екран: 6.67"</li>
-				<li>Камера: 108 MP</li>
-				<li>Памет: 128 GB</li>
-				<li>Цена: 500 лв</li>
-			</ul>
-		</div>
-	</div>
-
-	<div class="phone">
-		<div class="phone_img">
-			<img src="img/phone1.jpg">
-		</div>
-		
-		<div class="phone_txt">
-			<ul>
-				<li>Марка: Xiaomi</li>
-				<li>Модел: Redmi Note 11 Pro</li>
-				<li>Екран: 6.67"</li>
-				<li>Камера: 108 MP</li>
-				<li>Памет: 128 GB</li>
-				<li>Цена: 500 лв</li>
-			</ul>
-		</div>
-	</div>
-
-	<div class="phone">
-		<div class="phone_img">
-			<img src="img/phone1.jpg">
-		</div>
-		
-		<div class="phone_txt">
-			<ul>
-				<li>Марка: Xiaomi</li>
-				<li>Модел: Redmi Note 11 Pro</li>
-				<li>Екран: 6.67"</li>
-				<li>Камера: 108 MP</li>
-				<li>Памет: 128 GB</li>
-				<li>Цена: 500 лв</li>
-			</ul>
-		</div>
-	</div>
-
-	<div class="phone">
-		<div class="phone_img">
-			<img src="img/phone1.jpg">
-		</div>
-		
-		<div class="phone_txt">
-			<ul>
-				<li>Марка: Xiaomi</li>
-				<li>Модел: Redmi Note 11 Pro</li>
-				<li>Екран: 6.67"</li>
-				<li>Камера: 108 MP</li>
-				<li>Памет: 128 GB</li>
-				<li>Цена: 500 лв</li>
-			</ul>
-		</div>
-	</div>
-
-	<div class="phone">
-		<div class="phone_img">
-			<img src="img/phone1.jpg">
-		</div>
-		
-		<div class="phone_txt">
-			<ul>
-				<li>Марка: Xiaomi</li>
-				<li>Модел: Redmi Note 11 Pro</li>
-				<li>Екран: 6.67"</li>
-				<li>Камера: 108 MP</li>
-				<li>Памет: 128 GB</li>
-				<li>Цена: 500 лв</li>
-			</ul>
-		</div>
-	</div>
+	<?php endforeach ?>
 </div>
